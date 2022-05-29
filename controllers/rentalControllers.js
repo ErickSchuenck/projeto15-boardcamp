f0025c46ea75a45f77f605acbb6a837e808915a5
ErickSchuenck <esfmokdeci@gmail.com>
2022-05-28 23:15:05 -0300

    controllers feitos

--- controllers/rentalControllers.js
+++ controllers/rentalControllers.js
@@ -1,1 +1,19 @@
 import connection from '../db.js'
+
+export async function getRentals(req, res) {
+  if (req.params) {
+    try {
+      await connection.query(
+        `
+        `
+      )
+      res.send()
+    }
+    catch (error) {
+      res.send(error)
+    }
+  }
+};
+
+export async function postRentals(req, res) {
+};
